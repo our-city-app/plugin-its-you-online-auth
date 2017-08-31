f0bddfd2f40da268abdc63e47a71d298a2878259
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2017-08-31 13:53:55 +0200

    fix: Fix not being able to redo an api call in case the call failed

--- client/effects/organizations.effect.ts
+++ client/effects/organizations.effect.ts
@@ -4,7 +4,7 @@ import { Actions, Effect } from '@ngrx/effects';
 // libs
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import * as organizationActions from '../actions/organizations.action';
+import * as actions from '../actions/organizations.action';
 import { Organization } from '../index';
 // module
 import { OrganizationsService } from '../services/organizations.service';
@@ -13,36 +13,33 @@ import { OrganizationsService } from '../services/organizations.service';
 export class OrganizationsEffects {
 
   @Effect() getOrganizations$: Observable<Action> = this.actions$
-    .ofType(organizationActions.ActionTypes.GET_ORGANIZATIONS)
-    .switchMap(() => this.organizationsService.getOrganizations())
-    .map(payload => new organizationActions.GetOrganizationsCompleteAction(payload))
-    // nothing reacting to failure at moment but you could if you want (here for example)
-    .catch(() => Observable.of(new organizationActions.GetOrganizationsFailedAction()));
+    .ofType(actions.ActionTypes.GET_ORGANIZATIONS)
+    .switchMap(() => this.organizationsService.getOrganizations()
+      .map(payload => new actions.GetOrganizationsCompleteAction(payload))
+      .catch(() => Observable.of(new actions.GetOrganizationsFailedAction())));
 
   @Effect() getOrganization$: Observable<Action> = this.actions$
-    .ofType(organizationActions.ActionTypes.GET_ORGANIZATION)
-    .switchMap((value: Action) => this.organizationsService.getOrganization(value.payload))
-    .map((payload: Organization) => new organizationActions.GetOrganizationCompleteAction(payload))
-    // nothing reacting to failure at moment but you could if you want (here for example)
-    .catch(() => Observable.of(new organizationActions.GetOrganizationFailedAction()));
+    .ofType(actions.ActionTypes.GET_ORGANIZATION)
+    .switchMap((value: Action) => this.organizationsService.getOrganization(value.payload)
+      .map((payload: Organization) => new actions.GetOrganizationCompleteAction(payload))
+      .catch(() => Observable.of(new actions.GetOrganizationFailedAction())));
 
   @Effect() add$: Observable<Action> = this.actions$
-    .ofType(organizationActions.ActionTypes.ADD)
-    .switchMap((value: Action) => this.organizationsService.createOrganization(value.payload))
-    .map((payload: Organization) => new organizationActions.OrganizationAddedAction(payload));
+    .ofType(actions.ActionTypes.ADD)
+    .switchMap((value: Action) => this.organizationsService.createOrganization(value.payload)
+      .map((payload: Organization) => new actions.OrganizationAddedAction(payload)));
 
   @Effect() edit$: Observable<Action> = this.actions$
-    .ofType(organizationActions.ActionTypes.EDIT)
-    .switchMap((value: Action) => this.organizationsService.updateOrganization(value.payload))
-    .map(organization => new organizationActions.OrganizationEditedAction(organization))
-    .catch(() => Observable.of(new organizationActions.EditOrganizationFailedAction()))
-    .share();
+    .ofType(actions.ActionTypes.EDIT)
+    .switchMap((value: Action) => this.organizationsService.updateOrganization(value.payload)
+      .map(organization => new actions.OrganizationEditedAction(organization))
+      .catch(() => Observable.of(new actions.EditOrganizationFailedAction())));
 
   @Effect() delete$: Observable<Action> = this.actions$
-    .ofType(organizationActions.ActionTypes.DELETE)
-    .switchMap((value: Action) => this.organizationsService.deleteOrganization(value.payload))
-    .map(organization => new organizationActions.OrganizationRemovedAction(organization))
-    .catch((payload: Organization) => Observable.of(new organizationActions.RemoveOrganizationFailedAction(payload)));
+    .ofType(actions.ActionTypes.DELETE)
+    .switchMap((value: Action) => this.organizationsService.deleteOrganization(value.payload)
+      .map(organization => new actions.OrganizationRemovedAction(organization))
+      .catch((payload: Organization) => Observable.of(new actions.RemoveOrganizationFailedAction(payload))));
 
   constructor(private actions$: Actions,
               private organizationsService: OrganizationsService) {
