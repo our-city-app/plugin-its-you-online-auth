f83bf9f1a1f2c74034d8bf9d4dd22af4603edabd
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2019-02-16 12:39:41 +0100

    remove .ofType usage

--- client/effects/organizations.effect.ts
+++ client/effects/organizations.effect.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { of as observableOf } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import * as actions from '../actions/organizations.action';
@@ -8,35 +8,35 @@ import { OrganizationsService } from '../services';
 @Injectable()
 export class OrganizationsEffects {
 
-  @Effect() getOrganizations$ = this.actions$
-    .ofType<actions.GetOrganizationsAction>(actions.ActionTypes.GET_ORGANIZATIONS).pipe(
+  @Effect() getOrganizations$ = this.actions$.pipe(
+      ofType<actions.GetOrganizationsAction>(actions.ActionTypes.GET_ORGANIZATIONS),
       switchMap(() => this.organizationsService.getOrganizations().pipe(
         map(payload => new actions.GetOrganizationsCompleteAction(payload)),
         catchError(() => observableOf(new actions.GetOrganizationsFailedAction())),
       )));
 
-  @Effect() getOrganization$ = this.actions$
-    .ofType<actions.GetOrganizationAction>(actions.ActionTypes.GET_ORGANIZATION).pipe(
+  @Effect() getOrganization$ = this.actions$.pipe(
+      ofType<actions.GetOrganizationAction>(actions.ActionTypes.GET_ORGANIZATION),
       switchMap(action => this.organizationsService.getOrganization(action.payload).pipe(
         map(payload => new actions.GetOrganizationCompleteAction(payload)),
         catchError(() => observableOf(new actions.GetOrganizationFailedAction())),
       )));
 
-  @Effect() add$ = this.actions$
-    .ofType<actions.AddOrganizationAction>(actions.ActionTypes.ADD).pipe(
+  @Effect() add$ = this.actions$.pipe(
+      ofType<actions.AddOrganizationAction>(actions.ActionTypes.ADD),
       switchMap(action => this.organizationsService.createOrganization(action.payload).pipe(
         map(payload => new actions.OrganizationAddedAction(payload)),
       )));
 
-  @Effect() edit$ = this.actions$
-    .ofType<actions.EditOrganizationAction>(actions.ActionTypes.EDIT).pipe(
+  @Effect() edit$ = this.actions$.pipe(
+      ofType<actions.EditOrganizationAction>(actions.ActionTypes.EDIT),
       switchMap(action => this.organizationsService.updateOrganization(action.payload).pipe(
         map(organization => new actions.OrganizationEditedAction(organization)),
         catchError(() => observableOf(new actions.EditOrganizationFailedAction())),
       )));
 
-  @Effect() delete$ = this.actions$
-    .ofType<actions.RemoveOrganizationAction>(actions.ActionTypes.DELETE).pipe(
+  @Effect() delete$ = this.actions$.pipe(
+      ofType<actions.RemoveOrganizationAction>(actions.ActionTypes.DELETE),
       switchMap(action => this.organizationsService.deleteOrganization(action.payload).pipe(
         map(organization => new actions.OrganizationRemovedAction(organization)),
         catchError(payload => observableOf(new actions.RemoveOrganizationFailedAction(payload))),
